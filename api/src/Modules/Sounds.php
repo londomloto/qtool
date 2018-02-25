26b8403c79b6d1ce1c5a99d9e10f7c6088287bda
londomloto <roso.sasongko@gmail.com>
2018-02-26 05:06:39 +0700

    .

--- api/src/Modules/Sounds.php
+++ api/src/Modules/Sounds.php
@@ -38,25 +38,26 @@ class Sounds extends \QTool\Api\Libs\Module {
     public function items() {
         $path = urldecode($_GET['path']);
         $base = AUDIO_BASEPATH.$path;
-        
-        $scan = new \RecursiveIteratorIterator(
-            new \RecursiveDirectoryIterator($base, \RecursiveDirectoryIterator::SKIP_DOTS),
-            \RecursiveIteratorIterator::SELF_FIRST
-        );
-
         $data = array();
 
-        foreach($scan as $item) {
-            if ( ! $item->isDir() && $item->getExtension() == 'mp3') {
-                $name = $item->getFilename();
+        if (file_exists($base)) {
+            $scan = new \RecursiveIteratorIterator(
+                new \RecursiveDirectoryIterator($base, \RecursiveDirectoryIterator::SKIP_DOTS),
+                \RecursiveIteratorIterator::SELF_FIRST
+            );
+            foreach($scan as $item) {
+                if ( ! $item->isDir() && $item->getExtension() == 'mp3') {
+                    $name = $item->getFilename();
 
-                $data[] = array(
-                    'path' => 'api/sounds/play?path='.urlencode($path.'/'.$name),
-                    'text' => $name,
-                    'play' => FALSE
-                );
+                    $data[] = array(
+                        'path' => 'api/sounds/play?path='.urlencode($path.'/'.$name),
+                        'text' => $name,
+                        'play' => FALSE
+                    );
+                }
             }
         }
+        
 
         return array(
             'data' => $data
