449896d1e242ec239f40805ab62d2621f5178ffc
londomloto <roso.sasongko@gmail.com>
2018-03-01 01:31:39 +0700

    .

--- api/src/Modules/Assets.php
+++ api/src/Modules/Assets.php
@@ -4,8 +4,8 @@ namespace QTool\Api\Modules;
 class Assets extends \QTool\Api\Libs\Module {
 
     public function thumb() {
-        $maxw = 1366;
-        $maxh = 768;
+        $maxw = 450;
+        $maxh = 450;
         $path = urldecode($this->request->get('path'));
         $file = BASEPATH.$path;
 
