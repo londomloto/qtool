ce39a79e700dc20867922a53e4fa722edc4e9d28
londomloto <roso.sasongko@gmail.com>
2018-03-01 01:15:09 +0700

    .

--- api/src/Modules/Gallery.php
+++ api/src/Modules/Gallery.php
@@ -90,7 +90,7 @@ class Gallery extends \QTool\Api\Libs\Module {
 
     public function upload() {
         if (isset($_FILES['userfile'])) {
-            $path = 'temp/upload_'.md5_file($_FILES['userfile']['tmp_name']);
+            $path = 'temp/image.jpg';
             $temp = BASEPATH.$path;
             if (move_uploaded_file($_FILES['userfile']['tmp_name'], $temp)) {
                 return array(
